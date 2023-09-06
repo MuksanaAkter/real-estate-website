a00da3f2db0362e56a9ec22b7f5546c047be85ac
Muksana <muksanaakter3@gmail.com>
2023-09-06 23:53:45 +0600

    company

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../componants/Header/Header'
 import { Hero } from '../componants/Hero/Hero'
+import { Company } from '../componants/Company/Company'
 
 export const Home = () => {
   return (
    <div>
      <Header></Header>
      <Hero></Hero>
+     <Company></Company>
    </div>
   )
 }
