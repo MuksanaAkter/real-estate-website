c6321bf3be97b6fe5abcc7719298c8329f6dbec6
Muksana <muksanaakter3@gmail.com>
2023-09-07 23:49:31 +0600

    company

--- src/componants/Company/Company.jsx
+++ src/componants/Company/Company.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const Company = () => {
   return (
-    <div>Company</div>
-  )
-}
+    <div className="bg-gray-100">
+      <div className=" max-w-screen-xl mx-auto px-5 py-10 grid grid-cols-4 justify-center gap-5 items-center">
+        <img  className ="h-32" src="./prologis.png" alt="" />
+        <img className ="h-32"  src="./tower.png" alt="" />
+        <img className ="h-32"  src="./equinix.png" alt="" />
+        <img className ="h-32"  src="./realty.png" alt="" />
+      </div>
+    </div>
+  );
+};
